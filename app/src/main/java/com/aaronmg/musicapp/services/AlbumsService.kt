024f252a0f6c9b0fd6437831dd6e36563ca2c217
Aarón <amg81424@lasallebajio.edu.mx>
2026-05-09 13:24:02 -0600

    Se crea servicio y endpoints

---
+++ app/src/main/java/com/aaronmg/musicapp/services/AlbumsService.kt
@@ -0,0 +1,13 @@
+package com.aaronmg.musicapp.services
+
+import com.aaronmg.musicapp.models.Album
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface AlbumsService {
+    @GET("api/albums")
+    suspend fun getAllAlbums(): List<Album>
+
+    @GET("api/albums/{id}")
+    suspend fun getProductById( @Path("id") id: String)
+}
